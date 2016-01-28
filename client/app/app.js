02b224d3707b6878a3aa5afc1214e70c79a07eb4
Joey Leung <joeyleung8@gmail.com>
2016-01-28 14:51:28 -0800

    Added ngView/Controller, getUser api, getUser method

--- client/app/app.js
+++ client/app/app.js
@@ -3,14 +3,19 @@ angular.module('fickle', ['fickle.auth', 'fickle.services', 'fickle.search','ui.
 
  $urlRouterProvider.otherwise("/search");
  $stateProvider
-   .state('search', {
-     url: "/search",
-     templateUrl: "/app/Search/search.html",
-     controller: 'searchController'
-   })
-   .state('login', {
-     url: "/login",
-     templateUrl: "/app/login/login.html",
-     controller: 'AuthController'
-   })
-})
+  .state('search', {
+    url: "/search",
+    templateUrl: "/app/Search/search.html",
+    controller: 'searchController'
+  })
+  .state('login', {
+    url: "/login",
+    templateUrl: "/app/login/login.html",
+    controller: 'AuthController'
+    })
+  .state('user', {
+    url: "/user",
+    templateUrl: "/app/user/user.html",
+    controller: 'userController'
+  });
+});
