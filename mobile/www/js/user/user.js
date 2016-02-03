a1d149e6a41fd56f4836fc65e16eaaed9ff17a6d
Joey Leung <joeyleung8@gmail.com>
2016-02-02 18:22:54 -0800

    Added audio player

--- mobile/www/js/user/user.js
+++ mobile/www/js/user/user.js
@@ -1,21 +1,31 @@
 angular.module('enki.user',[])
 
-.controller('userController', function ($scope, $window, $location, User) {
+.controller('userController', function ($scope, $window, $location, User, $sce) {
   // console.log("ITEM: ", $window.localStorage.getItem('com.fickle'));
+  var b = ['http://feedproxy.google.com/~r/kabbalahmedia/eng/~5/DuhUhjCB0ms/eng_t_rav_bs-ester-ve-gilui_2016-02-02_lesson.mp3',
+  'http://www.podtrac.com/pts/redirect.mp3/media.devchat.tv/adventures-in-angular/AiA078NGBeta.mp3?rss=true'];
+  var a = ["dog","cat"];
   $scope.likedResources = [];
   $scope.dislikedResources = [];
-
+  $scope.play = a[0];
   User.getUser($window.localStorage.getItem('com.fickle'))
   .then(function (user) {
   });
 
-	  User.getLikes($window.localStorage.getItem('com.fickle')).then(function(res) {
-	    $scope.likedResources = res;
-	    console.log("hey", res);
-	  });
+    User.getLikes($window.localStorage.getItem('com.fickle')).then(function(res) {
+      $scope.likedResources = res;
+      console.log("hey", res);
+    });
 
   User.getDislike($window.localStorage.getItem('com.fickle')).then(function(data){
     $scope.dislikedResources = data;
     console.log("dislike", data);
-  })
+  });
+
+  $scope.changeSrc = function() {
+    var audio = document.getElementById('audio');
+    $scope.play = $sce.trustAsResourceUrl(b[1]);
+    audio.load();
+  };
+
 });
